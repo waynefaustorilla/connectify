ccfb52c4bc242ba753ba185cf40bfdf913f44f49
Wayne Faustorilla <wf@cloudeksperten.dk>
2026-03-19 08:09:29 +0000

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+  <title>{{ config("app.name", "Laravel Basics") }}</title>
+
+  @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @endif
+</head>
+
+<body>
+  <h1>Hello Nigga</h1>
+</body>
+
+</html>
